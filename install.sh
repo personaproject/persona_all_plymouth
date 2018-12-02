cf4c843ac0319fa7c731f080579ff32b420c66ef
me.msyariefh <45348110+personaproject@users.noreply.github.com>
2018-12-02 16:40:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 sudo cp -i -r persona_bar /usr/share/plymouth/themes/
 sudo cp -i -r persona_bar_text /usr/share/plymouth/themes/
 sudo cp -i -r persona_circle /usr/share/plymouth/themes/
-sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/persona/persona_bar.plymouth 100
-sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/persona/persona_bar.plymouth 100
-sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/persona/persona_circle.plymouth 100
+sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/persona_bar/persona_bar.plymouth 100
+sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/persona_bar_text/persona_bar_text.plymouth 100
+sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/persona_circle/persona_circle.plymouth 100
 sudo update-alternatives --config default.plymouth
 sudo update-initramfs -u

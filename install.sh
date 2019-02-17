aa1847e89733db5129c89aea4dbd5397c0fdf5a7
me.msyariefh <45348110+personaproject@users.noreply.github.com>
2019-02-17 12:07:33 +0700

    Update install.sh
    update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,27 @@
-sudo cp -i -r persona_bar /usr/share/plymouth/themes/
-sudo cp -i -r persona_bar_text /usr/share/plymouth/themes/
-sudo cp -i -r persona_circle /usr/share/plymouth/themes/
-sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/persona_bar/persona_bar.plymouth 100
-sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/persona_bar_text/persona_bar_text.plymouth 100
-sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/persona_circle/persona_circle.plymouth 100
-sudo update-alternatives --config default.plymouth
-sudo update-initramfs -u
-cd
-sudo rm -R persona_all_plymouth
-exit
+#!/bin/bash
+
+# Updated by Ryuffhant [ https://github.com/ryuffhant ]
+if ((${EUID:-0} || "$(id -u)")); then
+  clear
+  sleep 1.5
+  echo "You are not root"
+  sleep 0.5
+  echo "Please run using sudo command"
+  sleep 1.5
+  exit 1
+else
+  clear
+  sleep 1.5
+  echo "Now you're root user"
+  sleep 3
+  echo "Please be careful!!"
+  sleep 1.5
+  cp -iRv persona_bar /usr/share/plymouth/themes/
+  cp -iRv persona_bar_text /usr/share/plymouth/themes/
+  cp -iRv persona_circle /usr/share/plymouth/themes/
+  update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/persona_bar/persona_bar.plymouth 100
+  update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/persona_bar_text/persona_bar_text.plymouth 100
+  update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/persona_circle/persona_circle.plymouth 100
+  update-alternatives --config default.plymouth
+  update-initramfs -u
+fi
